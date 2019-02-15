bcb98126b62eb255a9f7b0a49f2e80132677ac79
jthae <40273807+jthae@users.noreply.github.com>
2019-02-14 23:42:30 -0600

    Added findNodes command to Main

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main
 	public static void main(String[] args) throws IOException 
 	{
 		HammingDist HammDist = new HammingDist("WEST", "BESS");
+		
+		HammDist.findNodes();
+		
 		System.out.println(HammDist);
 	}
 }
