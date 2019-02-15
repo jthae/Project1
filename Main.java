b85c217185513eb363564e59500fd1829bccab2b
jthae <40273807+jthae@users.noreply.github.com>
2019-02-14 23:43:30 -0600

    Added main comment

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 import java.io.IOException;
-
+/*
+* @author Jaycie Thaemert
+* @author Samson Olivero
+*
+* Main method to run Hamming Distance class.
+*/
 public class Main 
 {
 	public static void main(String[] args) throws IOException 
