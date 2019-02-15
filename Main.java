bce20434bf7672483bfaec20d367719a7ffbcaa6
jthae <40273807+jthae@users.noreply.github.com>
2019-02-14 23:44:15 -0600

    Added comments for main

--- Main.java
+++ Main.java
@@ -9,8 +9,10 @@ public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
+		// construct new hamming distance
 		HammingDist HammDist = new HammingDist("WEST", "BESS");
 		
+		// find nodes for both STID's
 		HammDist.findNodes();
 		
 		System.out.println(HammDist);
